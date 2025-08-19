0855e96db8d172b0e7c5579dd1f81ec5fbcd52c8
shimaenaga <admin@shimaenaga.dev>
2025-08-19 22:50:26 +0900

    Add Backend

---
+++ backend/go.mod
@@ -0,0 +1,36 @@
+module github.com/Kor-XCoder/DimiBoard/backend
+
+go 1.25.0
+
+require (
+	github.com/bytedance/sonic v1.14.0
+	github.com/gofiber/fiber/v3 v3.0.0-beta.5
+	github.com/gofiber/storage/redis/v3 v3.4.0
+)
+
+require (
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/bytedance/sonic/loader v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gofiber/schema v1.6.0 // indirect
+	github.com/gofiber/storage/testhelpers/redis v0.0.0-20250818120400-24709c108a34 // indirect
+	github.com/gofiber/utils/v2 v2.0.0-beta.13 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
+	github.com/redis/go-redis/v9 v9.12.1 // indirect
+	github.com/tinylib/msgp v1.3.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.64.0 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/net v0.42.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,92 @@
+package backend
+
+import (
+	"time"
+
+	"github.com/bytedance/sonic"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/compress"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/helmet"
+	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/storage/redis/v3"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		JSONEncoder: sonic.Marshal,
+		JSONDecoder: sonic.Unmarshal,
+	})
+
+	app.Use(helmet.New())
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowCredentials: true,
+	}))
+
+	app.Use(logger.New())
+	app.Use(compress.New())
+
+	storage := redis.New(redis.Config{
+		Host:     "127.0.0.1",
+		Port:     6379,
+		Password: "",
+	})
+
+	type Notice struct {
+		Title    string `json:"title"`
+		Type     string `json:"type"`
+		Deadline string `json:"deadline"`
+	}
+
+	app.Route("/notice").
+		Get(func(c fiber.Ctx) error {
+			var result []Notice
+			keys, err := storage.Keys()
+			if err != nil {
+				return err
+			}
+
+			for _, key := range keys {
+				value := new(Notice)
+				raw, err := storage.Get(string(key))
+				if err != nil {
+					return err
+				}
+				err = sonic.Unmarshal(raw, value)
+				if err != nil {
+					return err
+				}
+				result = append(result, Notice{
+					Title:    value.Title,
+					Type:     value.Type,
+					Deadline: value.Deadline,
+				})
+			}
+			return c.JSON(result)
+		}).
+		Post(func(c fiber.Ctx) error {
+			notice := new(Notice)
+			err := c.Bind().Body(notice)
+			if err != nil {
+				return err
+			}
+			data, err := sonic.Marshal(notice)
+			if err != nil {
+				return err
+			}
+			deadline, err := time.Parse("YYYY-MM-DD", notice.Deadline)
+			if err != nil {
+				return err
+			}
+			err = storage.Set(notice.Title, data, time.Until(deadline))
+			if err != nil {
+				return err
+			}
+			return c.JSON(notice)
+		})
+	app.Listen(":3000", fiber.ListenConfig{
+		EnablePrefork: true,
+	})
+}
