7847050ac030ce23cb8daad8ae555972f38ba01a
Kor-XCoder <mcoder1117@gmail.com>
2025-10-29 09:03:51 +0900

    web: 기말 날짜 업데이트

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -229,7 +229,7 @@ let lanes = reactive<BoardState>(loadState())
 onMounted(async () => {
   timer = window.setInterval(async () => {
     // 9/3까지의 남은 날짜 계산
-    const targetDate = new Date('2025-09-30T00:00:00+09:00'); // KST
+    const targetDate = new Date('2025-12-09T00:00:00+09:00'); // KST
     const now = new Date()
     const diff = targetDate.getTime() - now.getTime()
     const daysLeft = Math.ceil(diff / (1000 * 60 * 60 * 24))
